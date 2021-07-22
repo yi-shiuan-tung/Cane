ab82461830978e69b35e5c249b90dd1de3a25767
Yi-Shiuan Tung <yishiuant@gmail.com>
2021-07-22 15:43:44 -0600

    convert position from image frame to robot frame

--- segmentation/src/seg.py
+++ segmentation/src/seg.py
@@ -1,20 +1,35 @@
 #!/usr/bin/env python3
 import time
 from abc import ABC, abstractmethod
+from detectron2.data import MetadataCatalog
+from detectron2.utils.visualizer import Visualizer
 from typing import TYPE_CHECKING, List, Optional, Tuple, Union
 
 import message_filters
 import numpy as np
 import rospy
-#  import segmentation_models_pytorch as smp
 import torch
 import torch.nn as nn
 from cv_bridge import CvBridge
 from detectron2.config import get_cfg
 from detectron2.engine import DefaultPredictor
 from segmentation.msg import Prediction
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
 from torch import Tensor
+from detectron2 import model_zoo
+from scipy import ndimage, stats
+from image_geometry import PinholeCameraModel
+import tf2_ros
+import pyrealsense2
+from geometry_msgs.msg import Point, PointStamped
+import tf2_geometry_msgs
+
+
+REALSENSE_FOCAL_LENGTH = 1.93  # millimeters
+REALSENSE_SENSOR_HEIGHT_MM = 2.454  # millimeters
+REALSENSE_SENSOR_WIDTH_MM = 3.896  # millimeters
+REALSENSE_REZ_HEIGHT = 800  # pixels
+REALSENSE_REZ_WIDTH = 1280  # pixels
 
 if TYPE_CHECKING:
     from segmentation.detectron2.detectron2.config.config import CfgNode
@@ -65,8 +80,7 @@ class DetectronModel(DefaultPredictor, AbstractModel):
             config_file=config_file,
         )
         super().__init__(self.config)
-
-        self.class_names = self.metadata.class_names
+        self.class_names = MetadataCatalog.get("hrc_train").class_names
         self.num_classes = len(self.class_names)
 
     def setup_predictor_config(
@@ -79,13 +93,18 @@ class DetectronModel(DefaultPredictor, AbstractModel):
         """
         cfg = get_cfg()
 
-        cfg.merge_from_file(config_file)
+        cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
         cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
         # Mask R-CNN ResNet101 FPN weights
         cfg.MODEL.WEIGHTS = model_weights
         # This determines the resizing of the image. At 0, resizing is disabled.
         cfg.INPUT.MIN_SIZE_TEST = 0
         cfg.INPUT.FORMAT = "BGR"
+
+        cfg.SOLVER.IMS_PER_BATCH = 2
+        cfg.SOLVER.BASE_LR = 0.00025
+        cfg.SOLVER.MAX_ITER = 1000
+        cfg.MODEL.ROI_HEADS.NUM_CLASSES = 6
 
         return cfg
 
@@ -134,14 +153,88 @@ class DetectronModel(DefaultPredictor, AbstractModel):
 
     def full_output(
         self, x: Tensor
-    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, Tuple[str, ...]]:
+    ):
         pred = self.forward(x)["instances"]
         centers = pred.pred_boxes.get_centers().cpu().numpy()
         mask = pred.pred_masks.cpu().numpy().astype(np.uint8)
         labels = self.get_class_names(pred.pred_classes)
         scores = pred.scores.cpu().numpy()
 
-        return (mask, centers, labels, scores)
+        return mask, centers, labels, scores
+
+
+class DistanceInference:
+    def __init__(self):
+        self.bridge = CvBridge()
+
+    def get_distance(
+            self,
+            depth_map: np.ndarray,
+            mask: np.ndarray,
+            centers: np.ndarray,
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        r"""Get the distance of each object from a numpy array
+        Args:
+            mask (np.ndarray):  Binary masks of found objects
+            depth_map (np.ndarray):  Depth map input from camera
+            centers (np.ndarray):  Centers of objects, output from Detectron
+
+        Returns:
+            distances (np.ndarray): distances of each object
+            sizes (np.ndarray): sizes of each object
+            relative_positions (np.ndarray): positions of each object
+
+        Inputs:
+            - **mask** of shape `(H, W, 1)`, where each object in mask has different integer value.
+
+        Outputs:
+            - **relative_positions** of shape `(objects_found, 3)`, which are direction vectors
+                that tell where the object is in relation to the camera.
+        """
+        num_objects = mask.shape[0]
+
+        object_sizes = np.zeros((num_objects))
+        rel_positions = np.zeros((num_objects, 3))
+
+        fill_center = False
+        # only detectron model inputs the center of each object,
+        # thus if we get an empty array we must fill it
+        if centers.sum() == 0:
+            fill_center = True
+            centers = np.zeros((num_objects, 2))
+        else:
+            # Otherwise we have to unflatten array
+            centers = centers.reshape(num_objects, 2)
+
+        # rel_positions[i][1] is distances to objects
+        for i, obj_mask in enumerate(mask):
+            # Trimmed mean for distance to reject outliers
+            obj = obj_mask * depth_map
+            rel_positions[i][1] = stats.trim_mean(
+                obj[obj > 0], proportiontocut=0.2, axis=None
+            )
+
+            # Sum only width
+            object_px_size = np.max(obj_mask.sum(1))
+            obj_width_sensor = (REALSENSE_SENSOR_WIDTH_MM * object_px_size) / (
+                REALSENSE_REZ_WIDTH
+            )
+            object_sizes[i] = (
+                                      rel_positions[i][1] * obj_width_sensor
+                              ) / REALSENSE_FOCAL_LENGTH
+
+            # Calc width of FOV at known distance
+            field_width = (
+                                  REALSENSE_SENSOR_WIDTH_MM * rel_positions[i][1]
+                          ) / REALSENSE_FOCAL_LENGTH
+
+            if fill_center:
+                centers[i] = ndimage.measurements.center_of_mass(obj_mask)
+
+            rel_positions[i][0] = (centers[i][0] / REALSENSE_REZ_WIDTH) * field_width
+            rel_positions[i][2] = (centers[i][1] / REALSENSE_REZ_HEIGHT) * field_width
+
+        return object_sizes, rel_positions
 
 
 class SegmentationModel(nn.Module):
@@ -149,14 +242,15 @@ class SegmentationModel(nn.Module):
         self,
         model_name: str,
         weights: str,
-        rgb_topic: str,
-        depth_topic: str,
         config_file: str,
     ) -> None:
         super(SegmentationModel, self).__init__()
 
         if model_name not in AVAILABLE_MODELS:
             raise ValueError("Input model not available, select one from list")
+
+        MetadataCatalog.get("hrc_train").set(thing_classes=['chair-back', 'chair-back-cover', 'chair-bracket', 'leg',
+                                                            'seat', 'top'])
 
         # Create model
         # TODO: UPDATE timm model
@@ -165,44 +259,68 @@ class SegmentationModel(nn.Module):
         else:
             self.model = DetectronModel(model_weights=weights, config_file=config_file)
 
+        self.predictor = DefaultPredictor(self.model.config)
         self.pred_pub = rospy.Publisher("/seg/prediction", Prediction, queue_size=3)
-        rgb_sub = message_filters.Subscriber(rgb_topic, Image)
-        dep_sub = message_filters.Subscriber(depth_topic, Image)
-        synch = message_filters.ApproximateTimeSynchronizer([rgb_sub, dep_sub], 3, 0.1)
+        self.camera_info = rospy.wait_for_message("/camera/color/camera_info", CameraInfo)
+        self.camera_model = PinholeCameraModel()
+        self.camera_model.fromCameraInfo(self.camera_info)
+
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+        # self.trans = self.tf_buffer.lookup_transform("base", "camera_color_frame", rospy.Time(0), rospy.Duration(20.0))
+        rgb_sub = message_filters.Subscriber("/camera/color/image_raw", Image)
+        dep_sub = message_filters.Subscriber("/camera/depth/image_rect_raw", Image)
+        synch = message_filters.ApproximateTimeSynchronizer([rgb_sub, dep_sub], 5, 0.5)
         synch.registerCallback(self.callback)
         self.cv_bridge = CvBridge()
 
-    def callback(
-        self,
-        rgb: Image,
-        depth_map: Image,
-    ) -> None:
-        r"""ROS Subscriber to input RGB image + depth maps"""
-        print(
-            "SegmentationModel recieved message",
-            rgb.header.stamp,
-            depth_map.header.stamp,
-        )
-        rgb_img = self.cv_bridge.imgmsg_to_cv2(
-            rgb,
-            desired_encoding="passthrough",
-        )
+        self.distance_infer = DistanceInference()
+
+    def convert_depth_to_phys_coord_using_realsense(self, x, y, depth):
+        _intrinsics = pyrealsense2.intrinsics()
+        _intrinsics.width = self.camera_info.width
+        _intrinsics.height = self.camera_info.height
+        _intrinsics.ppx = self.camera_info.K[2]
+        _intrinsics.ppy = self.camera_info.K[5]
+        _intrinsics.fx = self.camera_info.K[0]
+        _intrinsics.fy = self.camera_info.K[4]
+        _intrinsics.model = pyrealsense2.distortion.none
+        _intrinsics.coeffs = [i for i in self.camera_info.D]
+        result = pyrealsense2.rs2_deproject_pixel_to_point(_intrinsics, [x, y], depth)
+        return result
+
+    def callback(self, rgb: Image, depth_map: Image) -> None:
+        r"""ROS Subscriber to input RGB image + depth maps
+        Args:
+            rgb (sensor_msgs.Image): Image message
+            depth_map (sensor_msgs.Image)
+        """
+        rgb_img = self.cv_bridge.imgmsg_to_cv2(rgb, desired_encoding="passthrough")
+        depth_img = self.cv_bridge.imgmsg_to_cv2(depth_map, desired_encoding="passthrough")
 
         (mask, centers, labels, scores) = self.model.full_output(rgb_img)
+        outputs = self.predictor(rgb_img)
+        v = Visualizer(rgb_img, metadata=MetadataCatalog.get("hrc_train"))
+        out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
 
         pub_img = Prediction()
-        pub_img.depth_map = depth_map
-        pub_img.mask = mask.ravel().tobytes()
-        pub_img.mask_height = mask.shape[2]
-        pub_img.mask_width = mask.shape[1]
-        pub_img.mask_channels = mask.shape[0]
         pub_img.header = rgb.header
         pub_img.scores = scores
         pub_img.centers = centers.ravel()
         pub_img.labels = labels
+        pub_img.labeled_img = self.cv_bridge.cv2_to_imgmsg(out.get_image(), encoding="passthrough")
 
-        #  end_t = time.perf_counter()
-        #  print("TIME TAKEN SEG NODE: ", end_t - start_t)
+        position_x = []
+        position_y = []
+        position_z = []
+        for center in centers:
+            vector = self.convert_depth_to_phys_coord_using_realsense(center[0], center[1], 1.45)
+            position_x.append(vector[0]+0.75)
+            position_y.append(-vector[1]-0.1)
+            position_z.append(vector[2])
+        pub_img.position_x = position_x
+        pub_img.position_y = position_y
+        pub_img.position_z = position_z
         self.pred_pub.publish(pub_img)
 
 
@@ -211,14 +329,10 @@ if __name__ == "__main__":
     weights = rospy.get_param("model_weights")
     model_type = rospy.get_param("segmentation_model")
     config = rospy.get_param("model_config")
-    rgb_input_topic = rospy.get_param("rgb_input_topic")
-    depth_input_topic = rospy.get_param("depth_input_topic")
     seg_model = SegmentationModel(
         model_name=model_type,
         weights=weights,
         config_file=config,
-        rgb_topic=rgb_input_topic,
-        depth_topic=depth_input_topic,
     )
 
     while not rospy.is_shutdown():
--- visualize/src/viz.py
+++ visualize/src/viz.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import cv2
 import message_filters
 import numpy as np
@@ -7,26 +7,18 @@ from cv_bridge import CvBridge
 from obj_inference.msg import Objects
 from segmentation.msg import Prediction
 from sensor_msgs.msg import Image
+import matplotlib.pyplot as plt
 
 bridge = CvBridge()
 
 
-def annotate_image(rgb, centers, labels, scores, dists):
+def annotate_image(rgb, centers, position_x, position_y, position_z):
     for i, center in enumerate(centers):
         rgb = cv2.circle(rgb, (int(center[0]), int(center[1])), 5, (0, 55, 255), -1)
         rgb = cv2.putText(
             rgb,
-            labels[i] + "{:.2f}".format(scores[i]),
-            fontScale=0.65,
-            org=(int(center[0]), int(center[1])),
-            thickness=1,
-            color=(194, 66, 245),
-            fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL,
-        )
-        rgb = cv2.putText(
-            rgb,
-            "{:.2f}".format(dists[i]),
-            fontScale=0.65,
+            "{:.2f} {:.2f}".format(position_x[i], position_y[i]),
+            fontScale=0.5,
             org=(int(center[0]), int(center[1] + 20)),
             thickness=1,
             color=(194, 66, 245),
@@ -35,37 +27,22 @@ def annotate_image(rgb, centers, labels, scores, dists):
     return rgb
 
 
-def callback(pred, inf, rgb, dep):
-    print("Visualize recieved messages")
-    sizes = np.array(inf.sizes)
-    scores = np.array(inf.scores)
-    labels = inf.labels
-    positions = np.array(inf.positions).reshape(sizes.shape[0], 2)
+def callback(pred, rgb,):
+    # sizes = np.array(inf.sizes)
+    # scores = np.array(inf.scores)
+    # labels = inf.labels
+    # positions = np.array(inf.positions).reshape(sizes.shape[0], 2)
 
     rgb_img = bridge.imgmsg_to_cv2(rgb, desired_encoding="passthrough")
-    dep_img = bridge.imgmsg_to_cv2(dep, desired_encoding="passthrough")
+    labeled_img = bridge.imgmsg_to_cv2(pred.labeled_img, desired_encoding="passthrough")
 
-    height = pred.mask_height
-    width = pred.mask_width
-    chan = pred.mask_channels
-    centers = np.array(pred.centers).reshape(chan, 2)
-    mask = np.frombuffer(pred.mask, dtype=np.uint8).reshape(chan, width, height).sum(0)
-    rgb = annotate_image(rgb_img, centers, labels, scores, positions[:, 1])
-    masskk = cv2.applyColorMap(
-        cv2.normalize(
-            mask[:, :, np.newaxis], None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U
-        ),
-        cv2.COLORMAP_JET,
-    )
-    depppthh = cv2.applyColorMap(
-        cv2.normalize(dep_img, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U),
-        cv2.COLORMAP_JET,
-    )
-    divider = np.zeros((480, 10, 3), dtype=np.uint8)
-    concatenn = np.concatenate((masskk, divider, depppthh, divider, rgb), 1)
-    cv2.imshow("input", concatenn)
-    key = cv2.waitKey(20)
-    if key == 27:
+    centers = np.array(pred.centers).reshape(-1, 2)
+    rgb = annotate_image(rgb_img, centers, pred.position_x, pred.position_y, pred.position_z)
+    divider = np.zeros((rgb.shape[0], 10, 3), dtype=np.uint8)
+    concat = np.concatenate((labeled_img, divider, rgb), 1)
+    cv2.imshow("input", concat)
+    key = cv2.waitKey(200)
+    if key == 27 & 0xff:
         cv2.destroyAllWindows()
 
 
@@ -74,16 +51,32 @@ class Visualizer:
 
     def __init__(self):
         pred_sub = message_filters.Subscriber("/seg/prediction", Prediction)
-        inf_sub = message_filters.Subscriber("/inference/obj_inference", Objects)
+        # inf_sub = message_filters.Subscriber("/inference/obj_inference", Objects)
         rgb_sub = message_filters.Subscriber("/camera/color/image_raw", Image)
-        dep_sub = message_filters.Subscriber("/camera/depth/image_rect_raw", Image)
+        # dep_sub = message_filters.Subscriber("/camera/depth/image_rect_raw", Image)
 
         ts = message_filters.ApproximateTimeSynchronizer(
-            [pred_sub, inf_sub, rgb_sub, dep_sub],
-            10,
-            0.1,
+            [pred_sub, rgb_sub],
+            20,
+            1.0,
         )
         ts.registerCallback(callback)
+
+    # Define a function to show the image in an OpenCV Window
+    def show_image(self, img):
+        cv2.imshow("Image Window", img)
+        cv2.waitKey(3)
+
+        # Define a callback for the Image message
+    def image_callback(self, pred, img_msg):
+        # log some info about the image topic
+        rospy.loginfo(img_msg.header)
+
+        # Try to convert the ROS Image message to a CV2 Image
+        cv_image = bridge.imgmsg_to_cv2(img_msg, "passthrough")
+
+        # Show the converted image
+        self.show_image(cv_image)
 
 
 if __name__ == "__main__":
@@ -91,6 +84,5 @@ if __name__ == "__main__":
 
     if viz:
         rospy.init_node("visualize")
-        cv2.namedWindow("input")
         viz = Visualizer()
         rospy.spin()
